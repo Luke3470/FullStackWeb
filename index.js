8f97ca7fd66bc872c60513e2b11b683d2dadd562
Luke3470 <94189667+Luke3470@users.noreply.github.com>
2025-10-09 09:58:16 +0100

    ADD: All endpoints complete

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ let users = [];
 let next_id = 1;
 
 app.get("/",(req,res)=>{
-    res.send("Hello World")
+    res.send("I am Alive")
+});
+
+app.get("/users",(req, res)=>{
+   return res.status(200).send(users);
 });
 
 app.post("/users", (req,res)=>{
@@ -23,6 +27,34 @@ app.post("/users", (req,res)=>{
     return res.status(201).send(user);
 });
 
+app.delete("/users/:id",(req,res) =>{
+    let id = parseInt(req.params.id);
+
+    const user = users.find(temp=> temp.user_id === id);
+    if (!user) return res.status(404).send("No User Found")
+
+    const index = users.indexOf(user);
+    users.splice(index,1);
+
+    return res.status(200).send("User Deleted")
+});
+
+app.patch("/users/:id",(req, res) =>{
+    const user = users.find(temp => temp.user_id === id);
+    if (!user) return res.status(404).send("No User Found")
+
+    const index = users.indexOf(user);
+    users.index.name = req.body.name;
+
+    return res.status(200).send("User name of "+index+" to "+req.body.name)
+});
+
+app.get("/users/id",(req, res) =>{
+    const user = users.find(temp => temp.user_id === id);
+    if (!user) return res.status(404).send("No User Found")
+
+    return res.status(200).send(user);
+});
 
 app.listen(port,()=>{
    console.log("Listening on port: "+ port)
