6f1fa3707c82cd4cb9708d6b99a55b7cedd80ece
Luke3470 <94189667+Luke3470@users.noreply.github.com>
2025-10-16 09:37:46 +0100

    Reset: to Base lab1 completion

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ app.use(express.json())
 
 const port = 8080;
 
-let users = [];
+let items = [];
 let next_id = 1;
 
 app.get("/",(req,res)=>{
     res.send("I am Alive")
 });
 
-app.get("/users",(req, res)=>{
-   return res.status(200).send(users);
+app.get("/cart",(req, res)=>{
+   return res.status(200).send(items);
 });
 
-app.post("/users", (req,res)=>{
-    let user = {
-        user_id: next_id,
-        user_name: req.body.name
+app.post("/cart", (req,res)=>{
+    let item = {
+        item_id: next_id,
+        item_name: req.body.item_name,
+        item_price: req.body.item_price,
+        quantity: req.body.quantity
     };
 
-    users.push(user);
+    items.push(item);
     next_id = next_id + 1;
 
-    return res.status(201).send(user);
+    return res.status(201).send(item);
 });
 
 app.delete("/users/:id",(req,res) =>{
@@ -40,20 +42,21 @@ app.delete("/users/:id",(req,res) =>{
 });
 
 app.patch("/users/:id",(req, res) =>{
-    const user = users.find(temp => temp.user_id === id);
-    if (!user) return res.status(404).send("No User Found")
+    const item = items.find(temp => temp.user_id === id);
+    if (!item) return res.status(404).send("No User Found")
 
     const index = users.indexOf(user);
-    users.index.name = req.body.name;
 
-    return res.status(200).send("User name of "+index+" to "+req.body.name)
+    users.index.name = req.body.quantity;
+
+    return res.status(200).send("User name of "+index+" to " +req.body.quantity)
 });
 
-app.get("/users/id",(req, res) =>{
-    const user = users.find(temp => temp.user_id === id);
-    if (!user) return res.status(404).send("No User Found")
+app.get("/users/:id",(req, res) =>{
+    const item = items.find(temp => temp.items_id === id);
+    if (!item) return res.status(404).send("No User Found")
 
-    return res.status(200).send(user);
+    return res.status(200).send(item);
 });
 
 app.listen(port,()=>{
